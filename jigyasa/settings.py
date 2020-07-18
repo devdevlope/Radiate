2029d2f8353ad70fbb753dd5d72de61f90b83386
Chirag Shenoy <53508426+devdevlope@users.noreply.github.com>
2020-07-18 16:36:59 +0530

    Final Touches

--- jigyasa/settings.py
+++ jigyasa/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'questions.apps.QuestionsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'jigyasa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_database',
+        'USER': 'postgres',
+        'PASSWORD': 'captain123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
